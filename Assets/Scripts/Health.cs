8c40a5d45e2e3682b003497789ad9c788122af9d
operator-id <52078084+operator-id@users.noreply.github.com>
2020-11-05 00:17:01 +0200

    Health Bar

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] Slider healthSlider;
 
+     private void Start()
+    {
+        
+        healthSlider.maxValue = health;
+        healthSlider.gameObject.SetActive(false);
+        healthSlider.value = health;
+    }
+
+ 
     public void DealDamage(float damage)
     {
         health -= damage;
+        healthSlider.value -= damage;
+        StartCoroutine(ShowHealthBar(4.5f));
 
         if (health <= 0)
         {
-         
+
             StartCoroutine(Die());
 
         }
@@ -22,10 +35,17 @@ public class Health : MonoBehaviour
         Destroy(gameObject.GetComponent<BoxCollider2D>());
         yield return new WaitForSeconds(1);
         Destroy(gameObject);
-        
+
     }
     public float getHealth()
     {
         return health;
+    }
+
+    IEnumerator ShowHealthBar(float time)
+    {
+        healthSlider.gameObject.SetActive(true);
+        yield return new WaitForSeconds(time);
+        healthSlider.gameObject.SetActive(false);
     }
 }
